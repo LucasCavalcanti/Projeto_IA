bab4956e98a28f1bb11a9d1142cfc63079463cd5
Lucas Cavalcanti <lucas.rodrigues@virtus.ufcg.edu.br>
2018-12-13 17:47:48 -0300

    predict car

--- Car_Price_Prediction/car.R
+++ Car_Price_Prediction/car.R
@@ -1,8 +1,11 @@
 library(readr)
 library(caret)
 
-true_car_listings <- read_csv("/home/lucas/Documents/true_car_listings.csv")
-newSet <- true_car_listings[sample(nrow(true_car_listings), 10000), ]
+true_car_listings <- read_csv("/home/lucas/Downloads/true_car_listings.csv")
+newSet <- true_car_listings[sample(nrow(true_car_listings), 1000), ]
+newSet$City <- NULL
+newSet$State <- NULL
+newSet$Vin <- NULL
 set.seed(10)
 
 indexTrain <- createDataPartition(newSet$Price, p = 0.8, list = FALSE)
@@ -15,5 +18,10 @@ set.seed(20)
 fitControl <- trainControl(method = "repeatedcv", number = 10, repeats = 4)
 
 set.seed(30)
-fit <- train(Price ~. -Vin, data = carTrain, method = "knn", trControl = fitControl, na.action = na.omit)
+fit <- train(Price ~., data = carTrain, method = "knn", trControl = fitControl, na.action = na.omit)
+fit
+plot(fit)
+
+predict(fit, carTest)
+
 
