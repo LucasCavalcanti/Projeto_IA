b29a5190fac929d7891997f2b527a839f2c1b81a
LucasCavalcanti <lucascavalcanti223@gmail.com>
2018-12-12 22:38:22 -0300

    knn car prediction

--- Car_Price_Prediction/car.R
+++ Car_Price_Prediction/car.R
@@ -2,10 +2,18 @@ library(readr)
 library(caret)
 
 true_car_listings <- read_csv("/home/lucas/Documents/true_car_listings.csv")
-
+newSet <- true_car_listings[sample(nrow(true_car_listings), 10000), ]
 set.seed(10)
-inTrain <- createDataPartition(true_car_listings$Price, p = 0.8, list = FALSE)
 
-carTrain <- true_car_listings[inTrain,]
-carTest <- true_car_listings[-inTrain,]
+indexTrain <- createDataPartition(newSet$Price, p = 0.8, list = FALSE)
+
+carTrain <- newSet[indexTrain,]
+carTest <- newSet[-indexTrain,]
+
+
+set.seed(20)
+fitControl <- trainControl(method = "repeatedcv", number = 10, repeats = 4)
+
+set.seed(30)
+fit <- train(Price ~. -Vin, data = carTrain, method = "knn", trControl = fitControl, na.action = na.omit)
 
