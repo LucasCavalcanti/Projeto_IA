435b0966c695c359bd44a049953f6ffe3c446b45
LucasCavalcanti <lucascavalcanti223@gmail.com>
2018-12-14 12:05:15 -0300

    analise no treinamento e teste

--- Car_Price_Prediction/car.R
+++ Car_Price_Prediction/car.R
@@ -4,7 +4,9 @@ library(rpart)
 library(rpart.plot)
 library(Metrics)
 
+# Importing dataset
 true_car_listings <- read_csv("/home/lucas/Documents/true_car_listings.csv")
+
 newSet <- true_car_listings[sample(nrow(true_car_listings), 10000), ]
 newSet$City <- NULL
 newSet$State <- NULL
@@ -12,24 +14,26 @@ newSet$Vin <- NULL
 newSet$Make <- NULL
 newSet$Model <- NULL
 
+# Splitting the dataset in training set and testing set
 indexTrain <- createDataPartition(newSet$Price, p = 0.8, list = FALSE)
-
 car.train <- newSet[indexTrain,]
 car.test <- newSet[-indexTrain,]
 
 
-ctrl <- trainControl(method = "cv", number = 10)
+ctrl <- trainControl(method = "repeatedcv", number = 5, repeats = 10)
 
-
-model.knn <- train(Price ~., data = car.train, method = "knn", trControl = ctrl, na.action = na.omit)
+# Training and predict a knn model
+k <- expand.grid(k = seq(20, 100, length=20))
+model.knn <- train(Price ~., data = car.train, method = "knn", trControl = ctrl,tuneGrid = k, na.action = na.omit)
 model.knn
 ggplot(model.knn)
 
 knn.predict <- predict(model.knn, car.test)
 
 knn.results <- data.frame(car.test$Price, knn.predict)
-knn.results
+ggplot(knn.results)
 
+# Linear Regression
 model.lm <- train(Price ~., data = car.train, method = "lm", trControl = ctrl, na.action = na.omit)
 model.lm
 
@@ -38,13 +42,22 @@ lm.predict <- predict(model.lm, car.test)
 lm.results <- data.frame(car.test$Price, lm.predict)
 lm.results
 
-decision_tree_model <- rpart(Price ~., data = car.train);
-rpart.plot(decision_tree_model)
-text(decision_tree_model, pretty = 0, cex = 0.6)
-test_tree_predict = predict(decision_tree_model, newdata = car.test)
-rmse.tree <- rmse(car.test$Price, test_tree_predict)
-rmse.tree
+ggplot(lm.results)
 
-mods <- resamples(list(knn = model.knn, lm = model.lm))
+# Decision Tree
+decision_tree_model <- train(Price ~.,
+                  data = car.train,
+                  method = "rpart",
+                  trControl = ctrl)
+tree.predict <- predict(decision_tree_model, car.test)
+
+ggplot(decision_tree_model)
+
+# Training Analising
+mods <- resamples(list(knn = model.knn, lm = model.lm, rpart = decision_tree_model))
 summary(mods)
 
+# Test Analising
+postResample(pred = knn.predict, obs = newSet$Price)
+postResample(pred = lm.predict, obs = newSet$Price)
+postResample(pred = tree.predict, obs = newSet$Price)
